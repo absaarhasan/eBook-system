ef7d544133b88bb309990d1e6688ba2a9702debd
Syed A Hasan <[absaarcv@outlook.com]>
2015-10-10 11:05:13 +0100

    protractor setup

---
+++ e2e-tests/protractor.conf.js
@@ -0,0 +1,31 @@
+
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['*.js']
+};
+
+/*
+
+
+
+
+exports.config = {
+  allScriptsTimeout: 11000,
+
+  specs: [
+    '*.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  baseUrl: 'http://localhost:8000/app/',
+
+  framework: 'jasmine',
+
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
+*/
---
+++ e2e-tests/scenarios.js
@@ -0,0 +1,42 @@
+'use strict';
+
+describe('BOL app', function() {
+
+
+  it('should automatically redirect to /1/1 when location hash/fragment is empty', function() {
+    browser.get('http://localhost:5000');
+    expect(browser.getLocationAbsUrl()).toMatch("/1/1");
+  });
+
+/*
+  describe('view1', function() {
+
+    beforeEach(function() {
+      browser.get('index.html#/view1');
+    });
+
+
+    it('should render view1 when user navigates to /view1', function() {
+      expect(element.all(by.css('[ui-view] p')).first().getText()).
+        toMatch(/partial for view 1/);
+    });
+
+  });
+
+
+  describe('view2', function() {
+
+    beforeEach(function() {
+      browser.get('index.html#/view2');
+    });
+
+
+    it('should render view2 when user navigates to /view2', function() {
+      expect(element.all(by.css('[ui-view] p')).first().getText()).
+        toMatch(/partial for view 2/);
+    });
+
+  });
+  */
+
+});
